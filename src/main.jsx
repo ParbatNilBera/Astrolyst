8865f66a7ff15ea714647ff7bb2e4a2f141ef2cc
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-08-08 20:58:00 +0530

    bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { UserProvider } from "./Context/UserContext.jsx";
+
+// Default export → no curly braces
+import UserProvider from "./Context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
