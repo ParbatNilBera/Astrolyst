8775bb53b241602e17fca6f0bd5c2945c7c38b4e
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-08-08 21:02:32 +0530

    Updated user provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { Toaster } from "react-hot-toast";
 import MainRoutes from "./routes/MainRoutes";
+import UserProvider from "./Context/UserContext";
 
 const App = () => {
   return (
     <>
-      <MainRoutes />
-      <Toaster position="top-right" reverseOrder={false} />
+      <UserProvider>
+        <MainRoutes />
+        <Toaster position="top-right" reverseOrder={false} />
+      </UserProvider>
     </>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// Default export → no curly braces
-import UserProvider from "./Context/UserContext.jsx";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    <App />
   </StrictMode>
 );
