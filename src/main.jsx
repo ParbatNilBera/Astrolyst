79ae6cda11629d807dbaceb66abb081d2fd5f361
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-08-08 20:54:32 +0530

    Updated Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import UserProvider from "./Context/userContext.jsx";
+import { UserProvider } from "./Context/UserContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
