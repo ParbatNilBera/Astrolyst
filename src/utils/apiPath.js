98964a09f4ce3ec47ffb2d4e9a64a799c11c4eb2
Parbat Nil Bera <parbatnilbera743@gmail.com>
2025-08-09 15:34:19 +0530

    path updated

--- src/utils/apiPath.js
+++ src/utils/apiPath.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://astrolyst-backend.onrender.com";
-export const BASE_URL = "http://localhost:8100";
+export const BASE_URL = "https://astrolyst-backend.onrender.com";
+// export const BASE_URL = "http://localhost:8100";
 
 export const API_PATH = {
   AUTH: {
